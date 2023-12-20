0c4afad8509646c305b18a6a818c2a137c3ee908
DhairyaSethi <55102840+DhairyaSethi@users.noreply.github.com>
2023-12-20 15:42:27 +0530

    feat: support new dir structures

--- compile_interfaces.sh
+++ compile_interfaces.sh
@@ -8,3 +8,17 @@ declare -a pkgs=($(ls src/pkgs))
 for pkg in "${pkgs[@]}"; do
   cp -r src/pkgs/$pkg/interface/ tmp/interfaces/$pkg/
 done
+
+# libs which follow src/interface pattern
+declare -a libs=("staking-hub")
+
+for pkg in "${libs[@]}"; do
+  cp -r lib/$pkg/src/interface/ tmp/interfaces/$pkg/
+done
+
+# libs which follow contracts/interfaces pattern
+declare -a libs=("core-contracts")
+
+for pkg in "${libs[@]}"; do
+  cp -r lib/$pkg/contracts/interfaces/ tmp/interfaces/$pkg/
+done
